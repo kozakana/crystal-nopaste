e6760b8bb521a6f42370e86e8a968df175287dbb
kozakana <mori@pla-cole.co>
2019-02-23 01:23:15 +0900

    app begin to work

--- src/crystal-nopaste.cr
+++ src/crystal-nopaste.cr
@@ -1,20 +1,41 @@
 require "kemal"
+require "./db/app_db"
+
+URL = "sqlite3://./data/sqlite.db"
 
 get "/" do
   render "src/views/input.ecr"
 end
 
-get "/:id" do
-  render "src/views/show.ecr"
+get "/:id" do |env|
+  db = AppDB.new :sqlite3, {url: URL}
+
+  paste = db.get_paste env.params.url["id"]
+  if paste.is_a?(Nil)
+    env.response.status_code = 404
+  else
+    description = paste[:description]
+    render "src/views/show.ecr"
+  end
 end
 
 get "/raw/:id" do |env|
-  env.params.url["id"]
+  db = AppDB.new :sqlite3, {url: URL}
+
+  paste = db.get_paste env.params.url["id"]
+  if paste.is_a?(Nil)
+    env.response.status_code = 404
+  else
+    paste[:description]
+  end
 end
 
 post "/" do |env|
-  env.params.body["description"].as(String)
-  #env.redirect "/"
+  db = AppDB.new :sqlite3, {url: URL}
+
+  description = env.params.body["description"].as(String)
+  id = db.create_paste description
+  env.redirect "/#{id}"
 end
 
 Kemal.run
